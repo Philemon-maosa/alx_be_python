0f74bccd880ee0feb789ae7c6e0b4a6fefe6c10b
philemon <philemonmaosa@gmail.com>
2025-08-03 19:25:32 +0300

    i have added files

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,13 @@
+def safe_divide(numerator, denominator):
+	try:
+		num = float(numerator)
+		denom = float(denominator)
+
+		result = num / denom
+		return f"Result: {result}"
+
+	except ZeroDivisionError:
+		return "Error: Cannot divide by zero."
+
+	except ValueError:
+		retun "Error: Inputs must be numeric."
